5a611ef0bb5359c3cd733af7e49d618d5f6ac41d
Luca Zibrinyi <zvmluci@gmail.com>
2021-03-23 19:22:22 +0100

    próba

--- src/main/java/com/codecool/whattoprint/Challenge.java
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -2,6 +2,14 @@ package com.codecool.whattoprint;
 
 class Challenge {
     public static String decideAndSay(String[] words, int number) {
-        return words[0];
+        StringBuilder sb= new StringBuilder();
+        if (number % 3 == 0 && number % 7 != 0) {
+            sb.append(words[0]);
+        } else if (number % 3 != 0 && number % 7 == 0) {
+            sb.append(words[1]);
+        } else if (number % 3 == 0 && number % 7 == 0) {
+            sb.append(words[0]).append(words[1]);
+        }
+        return sb.toString();
     }
 }
